db76438564688bc6f5b3c0e66fefbad78c5d1fbb
lhwdev <lhwdev6@outlook.com>
2022-05-03 23:03:45 +0900

    some dbg thingy

--- src/task/actions/test.ts
+++ src/task/actions/test.ts
@@ -1,4 +1,15 @@
 import { create as createArtifactClient } from "./utils/artifact/artifact-client.ts";
+import * as core from "./utils/core/core.ts";
+
+// deno-lint-ignore require-await
+await core.group("Env", async () => {
+  for (const [key, value] of Object.entries(Deno.env.toObject())) {
+    if (key.toLowerCase().includes("token")) {
+      console.log(`${key}=(token)`);
+    }
+    console.log(`${key}=${value.slice(0, 8)}`);
+  }
+});
 
 const artifactClient = createArtifactClient();
 await Deno.writeTextFile("hello-world.txt", "hello, world!");
