6f01431d2e0a3fc5f392840ac8f298c89c16b5ef
lhwdev <lhwdev6@outlook.com>
2022-05-14 01:42:23 +0900

    something..?

--- src/task/actions/test.ts
+++ src/task/actions/test.ts
@@ -1,10 +1,4 @@
 import { create as createArtifactClient } from "../../libs/actions/artifact/artifact-client.ts";
-import * as core from "../../libs/actions/core/core.ts";
-
-// deno-lint-ignore require-await
-await core.group("Env", async () => {
-  console.log(Object.keys(Deno.env.toObject()));
-});
 
 const artifactClient = createArtifactClient();
 await Deno.writeTextFile("hello-world.txt", "hello, world!");
